7cdcc4d5590e68eda1d40772d00faa84e98106cc
Hayam Mostafa <142169839+Hayam-mostafa@users.noreply.github.com>
2026-04-24 05:37:15 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,47 +4,51 @@ import cv2
 import numpy as np
 import re
 
-'''load Models'''
 card_model   = YOLO('Models/card_detector.pt')
+nid_model     = YOLO('Models/nid_detector.pt')
 digit_model = YOLO('Models/digit_detector.pt')
-id_model     = YOLO('Models/nid_detector.pt')
 reader       = easyocr.Reader(['ar'], gpu=False)
 clahe       = cv2.createCLAHE(clipLimit=1.5, tileGridSize=(8,8)) 
 
 
-'''detect and crop the card from the input image'''
+#detect and crop the card from the input image
 def crop_card(image):
     prediction_results = card_model.predict(image, conf=0.5, iou=0.45)
     prediction_result  = prediction_results[0]
 
     if len(prediction_result.boxes) == 0:
-        raise ValueError("No card detected")
+        return None
 
     boxes = prediction_result.boxes
-    confidences = boxes.conf.cpu().numpy()
-    best_idx = np.argmax(confidences)
+    boxes_xyxy = boxes.xyxy.cpu().numpy()
+    confs = boxes.conf.cpu().numpy()
+    
+    areas = (boxes_xyxy[:, 2] - boxes_xyxy[:, 0]) * (boxes_xyxy[:, 3] - boxes_xyxy[:, 1])
 
-    box = boxes.xyxy[best_idx].cpu().numpy()
-    x1, y1, x2, y2 = box.astype(int)
+    scores = confs * areas
+    best_idx = np.argmax(scores)
 
     h, w = image.shape[:2]
+
+    x1, y1, x2, y2 = boxes_xyxy[best_idx].astype(int)
 
     x1 = max(0, x1)
     y1 = max(0, y1)
     x2 = min(w, x2)
     y2 = min(h, y2)
-    
+
     image_cropped = image[y1:y2, x1:x2].copy()
-       
+    
     return image_cropped
 
 
-'''correct the orientation of the cropped card'''
+#correct the orientation of the cropped card
 def correct_orientation(image_cropped):
     angles = [0, 90, 180, 270]
     best_angle = 0
     max_conf = 0
 
+    h, w = image_cropped.shape[:2]
     small = cv2.resize(image_cropped, (320, 320))
 
     for angle in angles:
@@ -57,73 +61,69 @@ def correct_orientation(image_cropped):
         else:
             rotated = small
 
-        results = id_model.predict(rotated, conf=0.4, verbose=False)[0]
+        results = nid_model.predict(rotated, conf=0.4, verbose=False)[0]
 
         if len(results.boxes) > 0:
             current_conf = results.boxes.conf.cpu().numpy().max()
+
             if current_conf > max_conf:
                 max_conf = current_conf
                 best_angle = angle
 
     if best_angle == 90:
-        final_img = cv2.rotate(small, cv2.ROTATE_90_CLOCKWISE)
+        return cv2.rotate(image_cropped, cv2.ROTATE_90_CLOCKWISE)
     elif best_angle == 180:
-        final_img = cv2.rotate(small, cv2.ROTATE_180)
+        return cv2.rotate(image_cropped, cv2.ROTATE_180)
     elif best_angle == 270:
-        final_img = cv2.rotate(small, cv2.ROTATE_90_COUNTERCLOCKWISE)
-    else:
-        final_img = small
+        return cv2.rotate(image_cropped, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
-    return final_img
+    return image_cropped
 
 
-'''correct the skew of the orientation corrected image'''
 def correct_skew(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray, 50, 150)
 
-    lines = cv2.HoughLines(edges, 1, np.pi / 180, 150)
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, threshold=120)
 
     if lines is None:
         return image
 
     angles = []
-    for rho, theta in lines[:, 0]:
+    for line in lines:
+        rho, theta = line[0]
         angle = (theta * 180 / np.pi) - 90
-        angles.append(angle)
+
+        if -20 < angle < 20:
+            angles.append(angle)
+
+    if len(angles) < 5:  
+        return image
 
     median_angle = np.median(angles)
 
-    if abs(median_angle) > 20:
-        return image
+    if abs(median_angle) < 2 or abs(median_angle) > 20:
+        return image  
 
-    h, w = gray.shape
+    (h, w) = image.shape[:2]
+    M = cv2.getRotationMatrix2D((w//2, h//2), median_angle, 1.0)
 
-    rad = np.deg2rad(median_angle)
-    new_w = int(abs(w * np.cos(rad)) + abs(h * np.sin(rad)))
-    new_h = int(abs(h * np.cos(rad)) + abs(w * np.sin(rad)))
-
-    M = cv2.getRotationMatrix2D((w // 2, h // 2), median_angle, 1)
-
-    M[0, 2] += (new_w - w) / 2
-    M[1, 2] += (new_h - h) / 2
-
-    rotated = cv2.warpAffine(
-        image,M,(new_w, new_h),flags=cv2.INTER_CUBIC,borderMode=cv2.BORDER_REPLICATE)
+    rotated = cv2.warpAffine(image, M, (w, h),flags=cv2.INTER_CUBIC,borderMode=cv2.BORDER_REPLICATE)
 
     return rotated
-
-
-'''detect and crop the ID region from the corrected image'''
+    
+    
+#detect and crop the ID region from the corrected image
 def crop_id_box(image, pad=5):
-    result = id_model.predict(image, conf=0.5, verbose=False)[0]
+    result = nid_model.predict(image, conf=0.5, verbose=False)[0]
 
     if len(result.boxes) == 0:
-        raise ValueError("ID region not detected")
+        return None
 
     boxes = result.boxes
     confs = boxes.conf.cpu().numpy()
     best_idx = np.argmax(confs)
+    
     bbox = boxes.xyxy[best_idx].cpu().numpy().astype(int)
 
     x1, y1, x2, y2 = bbox
@@ -134,11 +134,10 @@ def crop_id_box(image, pad=5):
     return id_cropped
 
 
+#Function to detect the national ID number using YOLO
 def detect_national_id(id_cropped):
-    gray = cv2.cvtColor(id_cropped, cv2.COLOR_BGR2GRAY)
-    gray_3 = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
 
-    results = digit_model.predict(gray_3, conf=0.3, iou=0.5, verbose=False)[0]
+    results = digit_model.predict(id_cropped, conf=0.3, iou=0.5, verbose=False)[0]
 
     if len(results.boxes) == 0:
         return ""
@@ -148,33 +147,36 @@ def detect_national_id(id_cropped):
     confs     = results.boxes.conf.cpu().numpy()
 
     if len(class_ids) > 14:
-        top14    = np.argsort(confs)[-14:]
-        boxes    = boxes[top14]
-        class_ids = class_ids[top14]
+        num14    = np.argsort(confs)[-14:]
+        boxes    = boxes[num14]
+        class_ids = class_ids[num14]
 
     sorted_idx = np.argsort(boxes[:, 0])
-    return ''.join([str(int(class_ids[i])) for i in sorted_idx])
+    return ''.join([str(int(class_ids[i])) for i in sorted_idx]) 
 
 
+#Function to preprocess the cropped image for OCR
 def preprocess_image(image):
     gray     = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray_up  = cv2.resize(gray, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
-    denoised = cv2.fastNlMeansDenoising(gray_up, None, h=11)
+    denoised = cv2.fastNlMeansDenoising(gray_up, None, h=7)
     enhanced = clahe.apply(denoised)
-    blurred  = cv2.GaussianBlur(enhanced, (3,3), 0)
     binary   = cv2.adaptiveThreshold(
-        blurred, 255,
+        enhanced, 255,
         cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-        cv2.THRESH_BINARY, 31, 11
+        cv2.THRESH_BINARY, 31, 9
     )
+    kernel = np.ones((2, 2), np.uint8)
+    binary = cv2.morphologyEx(binary, cv2.MORPH_OPEN, kernel)
     return binary
 
 
-def convert_arabic(text):
-     return text.translate(str.maketrans("٠١٢٣٤٥٦٧٨٩", "0123456789"))
+#Function to convert Arabic digits to English digits
+def convert_arabic_to_english(text):
+    return text.translate(str.maketrans("٠١٢٣٤٥٦٧٨٩", "0123456789"))
 
 
-'''extract national ID number using OCR'''
+#Function to extract national ID number using OCR
 def extract_national_id(id_cropped):
     binary = preprocess_image(id_cropped)
     
@@ -190,55 +192,46 @@ def extract_national_id(id_cropped):
         text_threshold=0.2,
         low_text=0.2
     )
-
     
-    all_digits = ""
+    ocr_digits = ""
     for box, text, _ in sorted(result, key=lambda x: x[0][0][0]):
-        all_digits += re.sub(r'\D', '', convert_arabic(text))
+        ocr_digits += re.sub(r'\D', '', convert_arabic_to_english(text))
 
     yolo_digits = detect_national_id(id_cropped)
-
-    print(f"  OCR  : {all_digits} ({len(all_digits)}/14)")
-    print(f"  YOLO : {yolo_digits} ({len(yolo_digits)}/14)")
-
-    if all_digits == yolo_digits and len(all_digits) == 14:
-        print(" Match → Using YOLO")
+   
+    if len(yolo_digits) == 14:
         return yolo_digits
 
-    if len(all_digits) == 14 and len(yolo_digits) == 14:
-        matches = sum(a == b for a, b in zip(all_digits, yolo_digits))
-        print(f"Both detected ({matches}/14 match) → Using YOLO")
-        return yolo_digits
+    elif len(ocr_digits) == 14:
+        return ocr_digits
 
-    if len(all_digits) < 14 and len(yolo_digits) == 14:
-        print("OCR incomplete → YOLO")
-        return yolo_digits
-    
-    if len(all_digits) > 14 and len(yolo_digits) == 14:
-        print("OCR overdetected → YOLO")
-        return yolo_digits
-
-    if len(all_digits) == 14 and len(yolo_digits) < 14:
-        print("YOLO incomplete → OCR")
-        return all_digits
-
-    print(f"Failed , OCR={len(all_digits)} | YOLO={len(yolo_digits)}")
-    
-    return None
-
+    else:
+        return "Unverified"
 
 '''full pipeline from image to national ID number'''
-def full_pipeline(input_img):
+def extract_nid_from_image(input_img):
+
     img_array = np.frombuffer(input_img, np.uint8)
     image     = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
 
+    if image is None:                       
+        return None, None, "Invalid Image"
+ 
     image_cropped = crop_card(image)
+    
+    if image_cropped is None:
+        return image, None, "Card Not Detected"
 
     corrected_orientation = correct_orientation(image_cropped)
 
     corrected_skew = correct_skew(corrected_orientation)
 
     id_cropped = crop_id_box(corrected_skew)
+
+    if id_cropped is None:
+      return corrected_skew , None, "ID Not Detected"
+    
     nid = extract_national_id(id_cropped)
 
     return corrected_skew, id_cropped, nid
+
